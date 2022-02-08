88bf4759d4a6b93d2422b5840b8a6a070e7aa872
Felipe Augusto Carpes <fcarpes@imply.com>
2022-02-07 22:51:19 -0300

    componentizando aplicacao

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
-  // Complete aqui
+import { ReactChild, ReactFragment, ReactPortal } from "react"
+import { Key } from "react"
+import { MovieCard } from "./MovieCard"
+
+import '../styles/content.scss';
+
+
+export function Content(props: { selectedGenre: { title: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined }; movies: any[] }) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie: { imdbID: Key | null | undefined; Title: string; Poster: string; Runtime: string; Ratings: { Value: string }[] }) => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+export function SideBar(props: { genres: GenreResponseProps[]; handleClickButton: (arg0: number) => void; selectedGenreId: number; }) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre: GenreResponseProps) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+}
+
+// function genre(genre: any): import("react").ReactNode {
+//   throw new Error('Function not implemented.');
+// }
+
+
+// function handleClickButton(id: any): void {
+//   throw new Error('Function not implemented.');
+// }
